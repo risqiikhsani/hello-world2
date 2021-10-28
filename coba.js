1392d093936a59bd3d084294334fb2b7c8c387b0
risqiikhsani <48210802+risqiikhsani@users.noreply.github.com>
2021-10-28 22:22:01 +0700

    updating the cobagit.js

--- coba.js
+++ coba.js
@@ -1,10 +1,1 @@
-console.log("HALO")
-console.log("TEST")
-
-console.log("aku ditambahkan di branch2")
-
-console.log("aku ditambahkan di small-fix")
-console.log("aku ditambahkan di branch coba")
-
-
-console.log("UPDATE MORE")
+console.log("WE")
